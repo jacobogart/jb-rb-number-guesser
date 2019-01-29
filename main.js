5887f546bbdd6e08f0c0405c488086c350339bec
jacobogart <jacobogart@gmail.com>
2019-01-29 16:43:37 -0700

    Reformated setRange() and added for Challenger Name change.

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
+var randomNumber = generateRandomNumber();
 
 function setRange() {
-  // console.log("test");
   var minRange = parseInt(document.getElementById('min-range').value);
   var maxRange = parseInt(document.getElementById('max-range').value);
-  var randomNumber = Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
-  console.log(randomNumber);
+  randomNumber = generateRandomNumber(minRange, maxRange);
   document.getElementById('display-min').innerText = minRange;
   document.getElementById('display-max').innerText = maxRange;
 }
 
+function generateRandomNumber(min = 1, max = 100) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.getElementById('submit-btn').addEventListener('click', makeGuess);
+
+function makeGuess() {
+  var name1 = document.querySelector('#name1').value;
+  var name2 = document.querySelector('#name2').value;
+  var challengerNameOne = document.querySelectorAll('.challenger-name1');
+  for (var i = 0; i < challengerNameOne.length; i++) {
+    challengerNameOne[i].innerText = name1
+  }
+}
