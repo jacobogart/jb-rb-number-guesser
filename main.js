c01850392e19ca293bf0b824f6feebfc00dc9c88
jacobogart <jacobogart@gmail.com>
2019-01-29 14:37:41 -0700

    Challenge section will now update with current range.

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ function setRange() {
   // console.log("test");
   var minRange = parseInt(document.getElementById('min-range').value);
   var maxRange = parseInt(document.getElementById('max-range').value);
-  var randomNumber = Math.floor(Math.random() * maxRange) + minRange;
+  var randomNumber = Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
   console.log(randomNumber);
+  document.getElementById('display-min').innerText = minRange;
+  document.getElementById('display-max').innerText = maxRange;
 }
 
