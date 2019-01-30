95df24922728eed0701dedc3a467a668fd4a2a1b
jacobogart <jacobogart@gmail.com>
2019-01-30 15:15:54 -0700

    add correct guess card fuctionality

--- main.js
+++ main.js
@@ -45,16 +45,112 @@ function guessResultOne() {
   } else if (guess1 > randomNumber) {
     guessResultOne.innerText = 'that\'s too high';
   } else if (guess1 == randomNumber) {
-    correctGuess();
+    guessResultOne.innerText = 'BOOM!';
+    correctGuessOne();
   }
 }
 
+function guessResultTwo() {
+  var guessResultTwo = document.querySelector('#guess-result2');
+
+  if (guess2 < randomNumber) {
+    guessResultTwo.innerText = 'that\'s too low';
+  } else if (guess2 > randomNumber) {
+    guessResultTwo.innerText = 'that\'s too high';
+  } else if (guess2 == randomNumber) {
+    guessResultTwo.innerText = 'BOOM!';
+    correctGuessTwo();
+  }
+}
+
+function correctGuessOne() {
+  var right = document.querySelector('.right');
+  right.innerHTML += `<div class="winner-card">
+        <div class="flex jc-center top-line">
+          <h3 class="bold challenger-1">
+            ${name1}
+          </h3>
+          <p class="vs">
+            VS
+          </p>
+          <h3 class="bold challenger-2">
+            ${name2}
+          </h3>
+        </div>
+        <hr />
+        <div class="middle-line">
+          <h2 class="bold winner-name">
+            ${name1}
+          </h2>
+          <h2 class="winner">
+            Winner
+          </h2>
+        </div>
+        <hr />
+        <div class="flex jc-space-between bottom-line">
+          <h3 class="guesses">
+            <span class="bold guesses-number">47</span>
+            Guesses
+          </h3>
+          <h3 class="minutes">
+            <span class="bold minutes-number">1.35</span>
+            Minutes
+          </h3>
+          <img class="delete-btn" src="images/icons8-cancel-2.svg" alt="delete card button" />
+        </div>
+      </div>`
+  right.insertBefore(winnerCardOne, right.childNodes[0])
+
+}
+
+function correctGuessTwo() {
+  var right = document.querySelector('.right');
+  right.innerHTML += `<div class="winner-card">
+        <div class="flex jc-center top-line">
+          <h3 class="bold challenger-1">
+            ${name1}
+          </h3>
+          <p class="vs">
+            VS
+          </p>
+          <h3 class="bold challenger-2">
+            ${name2}
+          </h3>
+        </div>
+        <hr />
+        <div class="middle-line">
+          <h2 class="bold winner-name">
+            ${name2}
+          </h2>
+          <h2 class="winner">
+            Winner
+          </h2>
+        </div>
+        <hr />
+        <div class="flex jc-space-between bottom-line">
+          <h3 class="guesses">
+            <span class="bold guesses-number">47</span>
+            Guesses
+          </h3>
+          <h3 class="minutes">
+            <span class="bold minutes-number">1.35</span>
+            Minutes
+          </h3>
+          <img class="delete-btn" src="images/icons8-cancel-2.svg" alt="delete card button" />
+        </div>
+      </div>`
+
+  right.insertBefore(winnerCardTwo, right.childNodes[0])
+
+}
 
 function makeGuess() {
   updateName();
   updateGuess();
   guessResultOne();
-  console.log(randomNumber)
+  guessResultTwo();
+  correctGuessOne();
+  // console.log(randomNumber);
 }
 
 function isNumberKey(evt){
